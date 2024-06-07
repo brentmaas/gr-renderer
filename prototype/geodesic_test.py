39aa6d1a287c70505a58730b8f7659abb894a5d1
Brent Maas <brent.maas@hotmail.com>
2024-06-07 20:53:12 +0200

    Isotropic coordinates

--- prototype/geodesic_test.py
+++ prototype/geodesic_test.py
@@ -1,60 +1,57 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def pos_vel_to_polar(x, y, z, dx, dy, dz):
+def to_isotropic(x, y, z, dx, dy, dz, mass):
     r = np.sqrt(x ** 2 + y ** 2 + z ** 2)
-    theta = np.arccos(z / r)
-    phi = np.arctan2(y, x)
-    
-    v = np.sqrt(dx ** 2 + dy ** 2 + dz ** 2)
-    vtheta = np.arccos(dz / v)
-    vphi = np.arctan2(dy, dx)
-    
-    dr = np.sin(vtheta) * np.cos(vphi) * np.sin(theta) * np.cos(phi) + np.sin(vtheta) * np.sin(vphi) * np.sin(theta) * np.sin(phi) + np.cos(vtheta) * np.cos(theta)
-    dtheta = (np.sin(vtheta) * np.cos(vphi) * np.cos(theta) * np.cos(phi) + np.sin(vtheta) * np.sin(vphi) * np.cos(theta) * np.sin(phi) - np.cos(vtheta) * np.sin(theta)) / r
-    dphi = (-np.sin(vtheta) * np.cos(vphi) * np.sin(phi) + np.sin(vtheta) * np.sin(vphi) * np.cos(phi)) / (r * np.sin(theta))
-    
-    return r, theta, phi, dr, dtheta, dphi
+    r_iso = (np.sqrt(r) + np.sqrt(r - 2 * mass)) ** 2 / 4
+    x_iso = x / (1 + mass / 2 / r_iso) ** 2
+    y_iso = y / (1 + mass / 2 / r_iso) ** 2
+    z_iso = z / (1 + mass / 2 / r_iso) ** 2
+    dx_iso = dx / (1 + mass / 2 / r_iso) ** 2
+    dy_iso = dy / (1 + mass / 2 / r_iso) ** 2
+    dz_iso = dz / (1 + mass / 2 / r_iso) ** 2
+    f = mass / 2 / r_iso
+    dt = np.sqrt((1 + f) ** 6 / (1 - f) ** 2 * (dx_iso ** 2 + dy_iso ** 2 + dz_iso ** 2))
+    return x_iso, y_iso, z_iso, dx_iso, dy_iso, dz_iso, dt
 
-def pos_to_cartesian(r, theta, phi):
-    x = r * np.sin(theta) * np.cos(phi)
-    y = r * np.sin(theta) * np.sin(phi)
-    z = r * np.cos(theta)
-    
-    return x, y, z
-
-def christoffel_symbols(r, theta, phi, mass):
+def christoffel_symbols(x, y, z, mass):
     """
     Gamma^mu_rho_sigma -> array[mu][rho][sigma]
     mu, rho, sigma -> t, r, theta, phi
+    
+    From https://arxiv.org/pdf/0904.4184
     """
     
-    zero = np.zeros(len(r)) if type(r) == np.ndarray else 0
+    zero = np.zeros(len(x)) if type(x) == np.ndarray else 0
+    r = np.sqrt(x ** 2 + y ** 2 + z ** 2)
+    factor = mass / r ** 3
+    factor_plus = 1 + mass / r / 2
+    factor_minus = 1 - mass / r / 2
     return np.nan_to_num(np.array(
     [
         [ # t
-            [zero, mass / r / (r - 2 * mass), zero, zero], # t, t
-            [mass / r / (r - 2 * mass), zero, zero, zero], # t, r
-            [zero, zero, zero, zero], # t, theta
-            [zero, zero, zero, zero] # t, phi
+            [zero, x * factor / factor_minus, y * factor / factor_minus, z * factor / factor_minus], # t, t
+            [x * factor / factor_minus, zero, zero, zero], # t, x
+            [y * factor / factor_minus, zero, zero, zero], # t, y
+            [z * factor / factor_minus, zero, zero, zero] # t, z
         ],
-        [ # r
-            [mass * (r - 2 * mass) / r ** 3, zero, zero, zero], # r, t
-            [zero, -mass / r / (r - 2 * mass), zero, zero], # r, r
-            [zero, zero, 2 * mass - r, zero], # r, theta
-            [zero, zero, zero, (2 * mass - r) * np.sin(theta) ** 2] # r, phi
+        [ # x
+            [x * factor * factor_minus / factor_plus ** 7, zero, zero, zero], # x, t
+            [zero, -x * factor / factor_plus, -y * factor / factor_plus, -z * factor / factor_plus], # x, x
+            [zero, -y * factor / factor_plus, x * factor / factor_plus, zero], # x, y
+            [zero, -z * factor / factor_plus, zero, x * factor / factor_plus] # x, z
         ],
-        [ # theta
-            [zero, zero, zero, zero], # theta, t
-            [zero, zero, 1 / r, zero], # theta, r
-            [zero, 1 / r, zero, zero], # theta, theta
-            [zero, zero, zero, -np.sin(theta) * np.cos(theta)] # theta, phi
+        [ # y
+            [y * factor * factor_minus / factor_plus ** 7, zero, zero, zero], # y, t
+            [zero, y * factor / factor_plus, -x * factor / factor_plus, zero], # y, x
+            [zero, -x * factor / factor_plus, -y * factor / factor_plus, -z * factor / factor_plus], # y, y
+            [zero, zero, -z * factor / factor_plus, y * factor / factor_plus] # y, z
         ], 
-        [ # phi
-            [zero, zero, zero, zero], # phi, t
-            [zero, zero, zero, 1 / r], # phi, r
-            [zero, zero, zero, np.cos(theta) / np.sin(theta)], # phi, theta
-            [zero, 1 / r, np.cos(theta) / np.sin(theta), zero] # phi, phi
+        [ # z
+            [z * factor * factor_minus / factor_plus ** 7, zero, zero, zero], # z, t
+            [zero, z * factor / factor_plus, zero, -x * factor / factor_plus], # z, x
+            [zero, zero, z * factor / factor_plus, -y * factor / factor_plus], # z, y
+            [zero, -x * factor / factor_plus, -y * factor / factor_plus, -z * factor / factor_plus] # z, z
         ]
     ]))
 
@@ -69,7 +66,7 @@ def geodesic_leapfrog_step(pos, vel, daffine, mass):
                 dvel[mu] -= symbols[mu][rho][sigma] * vel[rho] * vel[sigma]
     nvel = vel + dvel * daffine
     
-    inside_horizon = npos[1] <= 2 * mass
+    inside_horizon = npos[1] * npos[1] + npos[2] * npos[2] + npos[3] * npos[3] <= mass ** 2 / 4
     nvel[:,inside_horizon] = vel[:,inside_horizon]
     npos[:,inside_horizon] = pos[:,inside_horizon]
     
@@ -81,16 +78,16 @@ vel = []
 rays = 12
 # Uniformly distributed rays from (3, 0)
 for i in range(rays):
-    r, theta, phi, dr, dtheta, dphi = pos_vel_to_polar(3, 0, 0, np.cos(2 * np.pi * i / rays), 0, np.sin(2 * np.pi * i / rays))
-    dt = np.sqrt(dr ** 2 / (1 - 2 * mass / r) ** 2 + r ** 2 / (1 - 2 * mass / r) * dtheta ** 2 + r ** 2 * np.sin(theta) ** 2 / (1 - 2 * mass / r) * dphi ** 2)
-    initial_pos.append([0, r, theta, phi])
-    vel.append([dt, dr, dtheta, dphi])
+    x, y, z, dx, dy, dz = 3.0, 0, 0, np.cos(2 * np.pi * i / rays), 0, np.sin(2 * np.pi * i / rays)
+    x_iso, y_iso, z_iso, dx_iso, dy_iso, dz_iso, dt = to_isotropic(x, y, z, dx, dy, dz, mass)
+    initial_pos.append([0, x_iso, y_iso, z_iso])
+    vel.append([dt, dx_iso, dy_iso, dz_iso])
 # Some sideways rays along the x-axis
-for x in [2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8, 2.9, 3.1, 3.2, 3.3, 3.4, 3.5, 3.6, 3.7, 3.8, 3.9, 4.0]:
-    r, theta, phi, dr, dtheta, dphi = pos_vel_to_polar(x, 0, 0, 0, 0, 1)
-    dt = np.sqrt(dr ** 2 / (1 - 2 * mass / r) ** 2 + r ** 2 / (1 - 2 * mass / r) * dtheta ** 2 + r ** 2 * np.sin(theta) ** 2 / (1 - 2 * mass / r) * dphi ** 2)
-    initial_pos.append([0, r, theta, phi])
-    vel.append([dt, dr, dtheta, dphi])
+for x0 in [2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8, 2.9, 3.1, 3.2, 3.3, 3.4, 3.5, 3.6, 3.7, 3.8, 3.9, 4.0]:
+    x, y, z, dx, dy, dz = x0, 0, 0, 0, 0, 1.0
+    x_iso, y_iso, z_iso, dx_iso, dy_iso, dz_iso, dt = to_isotropic(x, y, z, dx, dy, dz, mass)
+    initial_pos.append([0, x_iso, y_iso, z_iso])
+    vel.append([dt, dx_iso, dy_iso, dz_iso])
 
 steps = 2000
 daffine = 0.01
@@ -101,24 +98,30 @@ vel = np.array(vel).T
 for step in range(steps):
     pos[step+1], vel = geodesic_leapfrog_step(pos[step], vel, daffine, mass)
 
-x, y, z = pos_to_cartesian(pos[:,1], pos[:,2], pos[:,3])
+x_iso = pos[:,1]
+y_iso = pos[:,2]
+z_iso = pos[:,3]
+r_iso = np.sqrt(x_iso ** 2 + y_iso ** 2 + z_iso ** 2)
+x = x_iso * (1 + mass / 2 / r_iso) ** 2
+y = y_iso * (1 + mass / 2 / r_iso) ** 2
+z = z_iso * (1 + mass / 2 / r_iso) ** 2
 
 plt.figure()
 gridspec = plt.gcf().add_gridspec(3, 5)
 plt.gcf().add_subplot(gridspec[:3,:3])
-plt.gca().add_patch(plt.Circle((0, 0), 2, color="black", fill=False))
-plt.gca().add_patch(plt.Circle((0, 0), 3, color="black", linestyle="--", fill=False))
+plt.gca().add_patch(plt.Circle((0, 0), 2 * mass, color="black", fill=False))
+plt.gca().add_patch(plt.Circle((0, 0), 3 * mass, color="black", linestyle="--", fill=False))
 for i in range(pos.shape[2]):
-    plt.plot(x[:,i], z[:,i])
+    plt.plot(x, z)
 plt.gca().set_aspect("equal", adjustable="box")
 plt.gcf().add_subplot(gridspec[0,3:])
 for i in range(pos.shape[2]):
-    plt.plot(x[:,i])
+    plt.plot(x)
 plt.gcf().add_subplot(gridspec[1,3:])
 for i in range(pos.shape[2]):
-    plt.plot(y[:,i])
+    plt.plot(y)
 plt.gcf().add_subplot(gridspec[2,3:])
 for i in range(pos.shape[2]):
-    plt.plot(z[:,i])
+    plt.plot(z)
 plt.gcf().set_tight_layout(True)
 plt.show()
--- prototype/raytracing_test.py
+++ prototype/raytracing_test.py
@@ -2,68 +2,65 @@ import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 
-def pos_vel_to_polar(x, y, z, dx, dy, dz):
+def to_isotropic(x, y, z, dx, dy, dz, mass):
     r = np.sqrt(x ** 2 + y ** 2 + z ** 2)
-    theta = np.arccos(z / r)
-    phi = np.arctan2(y, x)
-    
-    v = np.sqrt(dx ** 2 + dy ** 2 + dz ** 2)
-    vtheta = np.arccos(dz / v)
-    vphi = np.arctan2(dy, dx)
-    
-    dr = np.sin(vtheta) * np.cos(vphi) * np.sin(theta) * np.cos(phi) + np.sin(vtheta) * np.sin(vphi) * np.sin(theta) * np.sin(phi) + np.cos(vtheta) * np.cos(theta)
-    dtheta = (np.sin(vtheta) * np.cos(vphi) * np.cos(theta) * np.cos(phi) + np.sin(vtheta) * np.sin(vphi) * np.cos(theta) * np.sin(phi) - np.cos(vtheta) * np.sin(theta)) / r
-    dphi = (-np.sin(vtheta) * np.cos(vphi) * np.sin(phi) + np.sin(vtheta) * np.sin(vphi) * np.cos(phi)) / (r * np.sin(theta))
-    
-    return r, theta, phi, dr, dtheta, dphi
-
-def pos_to_cartesian(r, theta, phi):
-    x = r * np.sin(theta) * np.cos(phi)
-    y = r * np.sin(theta) * np.sin(phi)
-    z = r * np.cos(theta)
-    
-    return x, y, z
+    r_iso = (np.sqrt(r) + np.sqrt(r - 2 * mass)) ** 2 / 4
+    x_iso = x / (1 + mass / 2 / r_iso) ** 2
+    y_iso = y / (1 + mass / 2 / r_iso) ** 2
+    z_iso = z / (1 + mass / 2 / r_iso) ** 2
+    dx_iso = dx / (1 + mass / 2 / r_iso) ** 2
+    dy_iso = dy / (1 + mass / 2 / r_iso) ** 2
+    dz_iso = dz / (1 + mass / 2 / r_iso) ** 2
+    f = mass / 2 / r_iso
+    dt = np.sqrt((1 + f) ** 6 / (1 - f) ** 2 * (dx_iso ** 2 + dy_iso ** 2 + dz_iso ** 2))
+    return x_iso, y_iso, z_iso, dx_iso, dy_iso, dz_iso, dt
 
 def normalise_cartesian(x, y, z):
     d = np.sqrt(x ** 2 + y ** 2 + z ** 2)
     return x / d, y / d, z / d
 
-def christoffel_symbols(r, theta, phi, mass):
+def christoffel_symbols(x, y, z, mass):
     """
     Gamma^mu_rho_sigma -> array[mu][rho][sigma]
     mu, rho, sigma -> t, r, theta, phi
+    
+    From https://arxiv.org/pdf/0904.4184
     """
     
-    zero = np.zeros(len(r)) if type(r) == np.ndarray else 0
+    zero = np.zeros(len(x)) if type(x) == np.ndarray else 0
+    r = np.sqrt(x ** 2 + y ** 2 + z ** 2)
+    factor = mass / r ** 3
+    factor_plus = 1 + mass / r / 2
+    factor_minus = 1 - mass / r / 2
     return np.nan_to_num(np.array(
     [
         [ # t
-            [zero, mass / r / (r - 2 * mass), zero, zero], # t, t
-            [mass / r / (r - 2 * mass), zero, zero, zero], # t, r
-            [zero, zero, zero, zero], # t, theta
-            [zero, zero, zero, zero] # t, phi
+            [zero, x * factor / factor_minus, y * factor / factor_minus, z * factor / factor_minus], # t, t
+            [x * factor / factor_minus, zero, zero, zero], # t, x
+            [y * factor / factor_minus, zero, zero, zero], # t, y
+            [z * factor / factor_minus, zero, zero, zero] # t, z
         ],
-        [ # r
-            [mass * (r - 2 * mass) / r ** 3, zero, zero, zero], # r, t
-            [zero, -mass / r / (r - 2 * mass), zero, zero], # r, r
-            [zero, zero, 2 * mass - r, zero], # r, theta
-            [zero, zero, zero, (2 * mass - r) * np.sin(theta) ** 2] # r, phi
+        [ # x
+            [x * factor * factor_minus / factor_plus ** 7, zero, zero, zero], # x, t
+            [zero, -x * factor / factor_plus, -y * factor / factor_plus, -z * factor / factor_plus], # x, x
+            [zero, -y * factor / factor_plus, x * factor / factor_plus, zero], # x, y
+            [zero, -z * factor / factor_plus, zero, x * factor / factor_plus] # x, z
         ],
-        [ # theta
-            [zero, zero, zero, zero], # theta, t
-            [zero, zero, 1 / r, zero], # theta, r
-            [zero, 1 / r, zero, zero], # theta, theta
-            [zero, zero, zero, -np.sin(theta) * np.cos(theta)] # theta, phi
+        [ # y
+            [y * factor * factor_minus / factor_plus ** 7, zero, zero, zero], # y, t
+            [zero, y * factor / factor_plus, -x * factor / factor_plus, zero], # y, x
+            [zero, -x * factor / factor_plus, -y * factor / factor_plus, -z * factor / factor_plus], # y, y
+            [zero, zero, -z * factor / factor_plus, y * factor / factor_plus] # y, z
         ], 
-        [ # phi
-            [zero, zero, zero, zero], # phi, t
-            [zero, zero, zero, 1 / r], # phi, r
-            [zero, zero, zero, np.cos(theta) / np.sin(theta)], # phi, theta
-            [zero, 1 / r, np.cos(theta) / np.sin(theta), zero] # phi, phi
+        [ # z
+            [z * factor * factor_minus / factor_plus ** 7, zero, zero, zero], # z, t
+            [zero, z * factor / factor_plus, zero, -x * factor / factor_plus], # z, x
+            [zero, zero, z * factor / factor_plus, -y * factor / factor_plus], # z, y
+            [zero, -x * factor / factor_plus, -y * factor / factor_plus, -z * factor / factor_plus] # z, z
         ]
     ]))
 
-def geodesic_leapfrog_step(pos, vel, daffine, mass, with_inside_horizon=False):
+def geodesic_leapfrog_step(pos, vel, daffine, mass):
     npos = pos + vel * daffine
     
     symbols = christoffel_symbols(pos[1], pos[2], pos[3], mass)
@@ -74,20 +71,20 @@ def geodesic_leapfrog_step(pos, vel, daffine, mass, with_inside_horizon=False):
                 dvel[mu] -= symbols[mu][rho][sigma] * vel[rho] * vel[sigma]
     nvel = vel + dvel * daffine
     
-    inside_horizon = npos[1] <= 2 * mass
+    inside_horizon = npos[1] * npos[1] + npos[2] * npos[2] + npos[3] * npos[3] <= mass ** 2 / 4
     nvel[:,inside_horizon] = vel[:,inside_horizon]
     npos[:,inside_horizon] = pos[:,inside_horizon]
     
-    if with_inside_horizon:
-        return npos, nvel, inside_horizon
-    return npos, nvel
+    return npos, nvel, inside_horizon
 
-def disk_transparency_at(r):
+def disk_transparency_at(r, disk_min, disk_max):
     return 1 / r / (1 / disk_min - 1 / disk_max) + 1 / (1 - disk_max / disk_min)
 
-def colour_step(col, pos, npos):
-    disk_intersect = np.logical_and.reduce(((npos[2] - np.pi / 2) * (pos[2] - np.pi / 2) < 0, npos[1] >= disk_min, npos[1] <= disk_max))
-    transparency = disk_transparency_at(npos[1,disk_intersect])
+def colour_step(col, pos, npos, mass, disk_min, disk_max):
+    nr_iso = np.sqrt(npos[1] * npos[1] + npos[2] * npos[2] + npos[3] * npos[3])
+    nr = nr_iso * (1 + mass / 2 / nr_iso) ** 2
+    disk_intersect = np.logical_and.reduce((npos[3] * pos[3] < 0, nr >= disk_min, nr <= disk_max))
+    transparency = disk_transparency_at(nr[disk_intersect], disk_min, disk_max)
     for i in range(3):
         col[i,disk_intersect] += disk_colour[i] * transparency * col[3,disk_intersect]
     col[3,disk_intersect] -= transparency * col[3,disk_intersect]
@@ -121,26 +118,24 @@ dz = np.sin(2 * (j.flatten() / size[1] - 0.5) * vfov)
 dx, dy, dz = normalise_cartesian(dx, dy, dz)
 dx, dz = dx * np.sin(start_theta) - dz * np.cos(start_theta), dz * np.sin(start_theta) + dx * np.cos(start_theta)
 dx, dy = dx * np.cos(start_phi) + dy * np.sin(start_phi), dy * np.cos(start_phi) - dx * np.sin(start_phi)
-initial_r, initial_theta, initial_phi, initial_dr, initial_dtheta, initial_dphi = pos_vel_to_polar(start_pos[0], start_pos[1], start_pos[2], dx, dy, dz)
-initial_dt = np.sqrt(initial_dr ** 2 / (1 - 2 * mass / initial_r) ** 2 + initial_r ** 2 / (1 - 2 * mass / initial_r) * initial_dtheta ** 2 + initial_r ** 2 * np.sin(initial_theta) ** 2 / (1 - 2 * mass / initial_r) * initial_dphi ** 2)
+x_iso, y_iso, z_iso, dx_iso, dy_iso, dz_iso, dt = to_isotropic(start_pos[0], start_pos[1], start_pos[2], dx, dy, dz, mass)
 
 steps = 1000
 daffine = -0.05
 pos = np.empty((steps + 1, 4, size[0] * size[1]))
 pos[0,0] = 0
-pos[0,1] = initial_r
-pos[0,2] = initial_theta
-pos[0,3] = initial_phi
-vel = np.array([initial_dt, initial_dr, initial_dtheta, initial_dphi])
+pos[0,1] = x_iso
+pos[0,2] = y_iso
+pos[0,3] = z_iso
+vel = np.array([dt, dx_iso, dy_iso, dz_iso])
 col = np.zeros((4, size[0] * size[1]))
 col[3] = 1
 inside_horizon = np.zeros(size[0] * size[1], dtype=bool)
 for step in tqdm(range(steps)):
-    pos[step+1], vel, inside_horizon = geodesic_leapfrog_step(pos[step], vel, daffine, mass, with_inside_horizon=True)
-    col = colour_step(col, pos[step], pos[step+1])
+    pos[step+1], vel, inside_horizon = geodesic_leapfrog_step(pos[step], vel, daffine, mass)
+    col = colour_step(col, pos[step], pos[step+1], mass, disk_min, disk_max)
 col = colour_finalise(col, inside_horizon)
 
-x, y, z = pos_to_cartesian(pos[:,1], pos[:,2], pos[:,3])
 img = np.zeros((size[1], size[0], 3))
 for i in range(3):
     img[:,:,i] = col[i].reshape((size[1], size[0]))
